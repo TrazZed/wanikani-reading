7420db0af8c76183a175d3ac8e8bb6e379909aef
TrazZed <hayestristan26@gmail.com>
2025-02-10 04:16:42 +1000

    API call now retrieves correctly uses pagination to retrieve all vocab

--- backend/api.py
+++ backend/api.py
@@ -5,16 +5,18 @@ async def fetch_wanikani_data(apiKey: str, endpoint: str, params: dict = None):
     # Base url for Wanikani API
     baseUrl = "https://api.wanikani.com/v2"
 
-    #Add API key to header
+    # Add API key to header
     headers = {
         "Authorization": f"Bearer {apiKey}"
     }
 
-    #If no parameters, use empty dict
+    # If no parameters, use an empty dict
     if params is None:
         params = {}
-    
-    # Make get request to Wanikani API
+
+    allData = []
+
+    # Make initial GET request to WaniKani API
     async with httpx.AsyncClient() as client:
         response = await client.get(f"{baseUrl}/{endpoint}", headers=headers, params=params)
 
@@ -22,4 +24,20 @@ async def fetch_wanikani_data(apiKey: str, endpoint: str, params: dict = None):
         if response.status_code != 200:
             raise HTTPException(status_code=response.status_code, detail=f"Error fetching data: {response.text}")
         
-        return response.json()
+        data = response.json()
+        allData.extend(data['data'])
+
+        # Check for next page and continue fetching if it exists
+        while data['pages']['next_url']:
+            # Update params with the next URL for the next page of data
+            nextUrl = data['pages']['next_url']
+            response = await client.get(nextUrl, headers=headers)
+
+            # Check for successful request
+            if response.status_code != 200:
+                raise HTTPException(status_code=response.status_code, detail=f"Error fetching data: {response.text}")
+
+            data = response.json()
+            allData.extend(data['data'])
+
+        return allData
